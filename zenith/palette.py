645eb548bd186b043402716f6617ce8766859a51
bczsalba <bczsalba@gmail.com>
2023-10-23 12:20:35 +0200

    [palette]: Add `text` key to palette

--- zenith/palette.py
+++ zenith/palette.py
@@ -7,7 +7,7 @@ from typing import Callable, Tuple
 
 from slate.color import Color, color
 
-from .markup import MarkupContext, zml_alias
+from .markup import MarkupContext, zml_alias, GLOBAL_CONTEXT
 
 __all__ = [
     "triadic",
@@ -69,32 +69,18 @@ def tetradic(base: Color) -> tuple[Color, Color, Color, Color]:
     return base.tetradic_group
 
 
-DEFAULT_SURFACE = Color.black().blend_complement(0.2)
-SURFACE_BLEND_ALPHA = 0.2
+DEFAULT_PANEL = Color.black().blend_complement(0.2)
+PANEL_BLEND_ALPHA = 0.05
 
+DEFAULT_TEXT = Color((245, 245, 245))
 DEFAULT_SUCCESS = Color.from_hex("67eb7f")
 DEFAULT_ERROR = Color.from_hex("eb7067")
 DEFAULT_WARNING = Color.from_hex("ebe267")
+
 SEMANTIC_BLEND_ALPHA = 0.3
 
 
 class Palette:
-    namespace: str = ""
-
-    primary: Color
-    secondary: Color
-    tertiary: Color
-    quaternay: Color
-
-    success: Color
-    warning: Color
-    error: Color
-
-    surface1: Color
-    surface2: Color
-    surface3: Color
-    surface4: Color
-
     def __init__(
         self,
         primary: Color | str,
@@ -103,13 +89,14 @@ class Palette:
         strategy: PalettingFunction = triadic,
         shade_count: int = 3,
         shade_step: float = 0.1,
+        text: Color = DEFAULT_TEXT,
         secondary: Color | None = None,
         tertiary: Color | None = None,
         quaternary: Color | None = None,
         success: Color | None = None,
         warning: Color | None = None,
         error: Color | None = None,
-        surface_base: Color = DEFAULT_SURFACE,
+        panel_base: Color = DEFAULT_PANEL,
         success_base: Color = DEFAULT_SUCCESS,
         warning_base: Color = DEFAULT_WARNING,
         error_base: Color = DEFAULT_ERROR,
@@ -123,6 +110,8 @@ class Palette:
 
         _, g_secondary, g_tertiary, g_quaternary = strategy(primary)
 
+        self.text = text
+
         self.secondary = secondary or g_secondary
         self.tertiary = tertiary or g_tertiary
         self.quaternary = quaternary or g_quaternary
@@ -131,23 +120,24 @@ class Palette:
         self.warning = warning_base.blend(primary, SEMANTIC_BLEND_ALPHA)
         self.error = error_base.blend(primary, SEMANTIC_BLEND_ALPHA)
 
-        self.surface1 = surface_base.blend(self.primary, alpha=SURFACE_BLEND_ALPHA)
-        self.surface2 = surface_base.blend(self.secondary, alpha=SURFACE_BLEND_ALPHA)
-        self.surface3 = surface_base.blend(self.tertiary, alpha=SURFACE_BLEND_ALPHA)
-        self.surface4 = surface_base.blend(self.quaternary, alpha=SURFACE_BLEND_ALPHA)
+        self.panel1 = panel_base.blend(self.primary, alpha=PANEL_BLEND_ALPHA)
+        self.panel2 = panel_base.blend(self.secondary, alpha=PANEL_BLEND_ALPHA)
+        self.panel3 = panel_base.blend(self.tertiary, alpha=PANEL_BLEND_ALPHA)
+        self.panel4 = panel_base.blend(self.quaternary, alpha=PANEL_BLEND_ALPHA)
 
         self._keys = {
             "primary": self.primary,
+            "text": self.text,
             "secondary": self.secondary,
             "tertiary": self.tertiary,
             "quaternary": self.quaternary,
             "success": self.success,
             "error": self.error,
             "warning": self.warning,
-            "surface1": self.surface1,
-            "surface2": self.surface2,
-            "surface3": self.surface3,
-            "surface4": self.surface4,
+            "panel1": self.panel1,
+            "panel2": self.panel2,
+            "panel3": self.panel3,
+            "panel4": self.panel4,
         }
 
         self._mapping = {}
@@ -172,7 +162,7 @@ class Palette:
         self._shade_count = shade_count
 
     def alias(
-        self, ctx: MarkupContext | None, ignore_already_aliased: bool = False
+        self, ctx: MarkupContext | None = None, ignore_already_aliased: bool = False
     ) -> None:
         """Sets up aliases for the given context.
 
@@ -189,8 +179,7 @@ class Palette:
                 + " or set `ignore_already_aliased`"
             )
 
-        self._ctx = ctx
-
+        self._ctx = ctx or GLOBAL_CONTEXT
         zml_alias(**self._mapping, ctx=ctx)
 
     def unalias(self) -> None:
